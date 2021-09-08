df7f6fa71fe3d620d8eabbe9fc64e0c52e222c54
zabbott <zabbott904@gmail.com>
2021-09-08 13:06:49 -0500

    no message

---
+++ Assets/LightEstimation.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+public class LightEstimation : MonoBehaviour
+{
+    public Light MainLight;
+    public ARCameraManager ARCM;
+    public TextReceiver TR; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        ARCM.frameReceived += FrameUpdated;
+
+    }
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        ARCM.frameReceived -= FrameUpdated;
+    }
+    // Update is called once per frame
+ 
+
+    private void FrameUpdated(ARCameraFrameEventArgs args)
+    {
+        if (args.lightEstimation.averageBrightness.HasValue)
+        {
+            MainLight.intensity = args.lightEstimation.averageBrightness.Value;
+            TR.mytext.text = args.lightEstimation.averageBrightness.Value.ToString();
+        }
+        if (args.lightEstimation.averageColorTemperature.HasValue)
+        {
+            MainLight.colorTemperature = args.lightEstimation.averageColorTemperature.Value;
+        }
+        if (args.lightEstimation.mainLightColor.HasValue)
+        {
+            MainLight.color = args.lightEstimation.mainLightColor.Value;
+        }
+    }
+}
---
+++ Assets/TextReceiver.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TextReceiver : MonoBehaviour
+{
+    public TMP_Text mytext;
+    public Transform[] trackables;
+    public Light mainLight; 
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        string example = ""; 
+        //foreach(var item in trackables)
+        //{
+        //    example = example + item.name + "Pos" + item.position;
+        //}
+
+        //example = mainLight.intensity + " " + mainLight.colorTemperature + " " + mainLight.color;
+
+
+        //mytext.text = example;
+    }
+}
